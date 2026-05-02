35bad1fc808c783026c9e3bc39a5750373b886ff
Manangupta246 <manangupta246@gmail.com>
2026-05-02 15:55:16 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
+const nextConfig = { 
+  images: { 
     remotePatterns: [
-      { protocol: 'https', hostname: 'assets.zyrosite.com' },
-      { protocol: 'https', hostname: 'api.dicebear.com' },
-    ],
-  },
+      { protocol: "https", hostname: "d2lk14jtvqry1q.cloudfront.net" }, 
+      { protocol: "https", hostname: "api.dicebear.com" },
+      { protocol: "https", hostname: "lbrcrknnivxkqvryzamr.supabase.co" }
+    ] 
+  } 
 };
-
 module.exports = nextConfig;
