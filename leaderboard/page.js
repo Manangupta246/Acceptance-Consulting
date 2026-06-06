b74d3807a6c8893f4b4b6302be80a37f5bf8ccd6
Manangupta246 <manangupta246@gmail.com>
2026-06-06 14:36:44 +0400

    Delete leaderboard directory

--- leaderboard/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { LeaderboardPage } from "../components";
-import { useApp } from "../AppContext";
-export default function Page() {
-  var { user, onLoginClick, onOpenChat } = useApp();
-  return <LeaderboardPage user={user} onOpenChat={onOpenChat} onLoginClick={onLoginClick} />;
-}
