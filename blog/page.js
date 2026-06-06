8207bd4d47916c366ffa86746f14a26e541416b5
Manangupta246 <manangupta246@gmail.com>
2026-06-06 14:36:23 +0400

    Delete blog directory

--- blog/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { BlogPage } from "../components";
-import { useApp } from "../AppContext";
-export default function Page() {
-  var { user } = useApp();
-  return <BlogPage user={user} />;
-}
