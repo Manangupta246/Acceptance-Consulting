22ab21a9e560cb0dd7b8c72e09036a8bf947f9e3
Manangupta246 <manangupta246@gmail.com>
2026-06-06 14:34:36 +0400

    Delete admin directory

--- admin/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { AdminDashboard } from "../components";
-import { useApp } from "../AppContext";
-export default function Page() {
-  var { user } = useApp();
-  return <AdminDashboard user={user} />;
-}
