4b247d1380c406c64658f93dd978c0f92c59d201
Manangupta246 <manangupta246@gmail.com>
2026-06-06 14:37:12 +0400

    Delete study-partner directory

--- study-partner/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { AccountabilityPage } from "../components";
-import { useApp } from "../AppContext";
-export default function Page() {
-  var { user, onLoginClick, onOpenChat } = useApp();
-  return <AccountabilityPage user={user} onLoginClick={onLoginClick} onOpenChat={onOpenChat} />;
-}
