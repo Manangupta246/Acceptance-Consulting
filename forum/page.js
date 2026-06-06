e028ddbc3ac7fb9868a17ec9aa79634d151b4383
Manangupta246 <manangupta246@gmail.com>
2026-06-06 14:36:34 +0400

    Delete forum directory

--- forum/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { ForumPage } from "../components";
-import { useApp } from "../AppContext";
-export default function Page() {
-  var { user, onLoginClick } = useApp();
-  return <ForumPage user={user} onLoginClick={onLoginClick} />;
-}
