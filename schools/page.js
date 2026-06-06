0b34da7febc0c5d861eda8f525d4220a9a3f4827
Manangupta246 <manangupta246@gmail.com>
2026-06-06 14:36:51 +0400

    Delete schools directory

--- schools/page.js
+++
@@ -1,7 +0,0 @@
-"use client";
-import { SchoolFinderPage } from "../components";
-import { useApp } from "../AppContext";
-export default function Page() {
-  var { user, onLoginClick } = useApp();
-  return <SchoolFinderPage user={user} onLoginClick={onLoginClick} />;
-}
